448af5928b918914cefdab9337c5bbe69348eaf1
rakshithagp5 <97862550+rakshithagp5@users.noreply.github.com>
2022-01-25 14:54:00 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,69 +1,53 @@
-package ClassObjectsandPillarsoOOPs;
-abstract class Shape  
-{ 
-    String color; 
-    abstract double area(); 
-    public abstract String toString(); 
-    public Shape(String color) 
-    { 
-        System.out.println("Shape constructor called"); 
-        this.color = color; 
-    } 
-    public String getColor() 
-    { 
-        return color; 
-    } 
-} 
-class Circle extends Shape 
-{ 
-    double radius; 
-    public Circle(String color,double radius) 
-    { 
-        super(color); 
-        System.out.println("Circle constructor called"); 
-        this.radius = radius; 
+package FileHandlingMechanisms;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+public class TextFileModificationProgram
+{   
+    static void modifyFile(String filePath, String oldString, String newString)
+    {
+        File fileToBeModified = new File(filePath);
+        String oldContent = "";
+        BufferedReader reader = null;
+        FileWriter writer = null;
+        try
+        {
+            reader = new BufferedReader(new FileReader(fileToBeModified));
+            String line = reader.readLine();
+            while (line != null) 
+            {
+                oldContent = oldContent + line + System.lineSeparator();
+                line = reader.readLine();
+            }
+            String newContent = oldContent.replaceAll(oldString, newString);
+            writer = new FileWriter(fileToBeModified);
+            writer.write(newContent);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+                writer.close();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        }
     }
-    @Override
-    double area() 
-    { 
-        return Math.PI * Math.pow(radius, 2); 
-    } 
-    @Override
-    public String toString() 
-    { 
-        return "Circle color is " + super.color + "and area is : " + area(); 
-    } 
-} 
-class Rectangle extends Shape
-{ 
-    double length; 
-    double width; 
-    public Rectangle(String color,double length,double width) 
-    { 
-        super(color); 
-        System.out.println("Rectangle constructor called"); 
-        this.length = length; 
-        this.width = width; 
-    } 
-    @Override
-    double area() 
-    { 
-        return length*width; 
-    } 
-    @Override
-    public String toString() 
-    { 
-        return "Rectangle color is " + super.color +  
-                           "and area is : " + area(); 
-    } 
-} 
-public class Test  
-{ 
-    public static void main(String[] args) 
-    { 
-        Shape s1 = new Circle("Red", 2.2); 
-        Shape s2 = new Rectangle("Yellow", 2, 4);
-        System.out.println(s1.toString()); 
-        System.out.println(s2.toString()); 
-    } 
+    public static void main(String[] args)
+    {
+        modifyFile("c://temp//testFile2.txt", "85", "95");
+        System.out.println("done");
+    }
 }
+
+
